7b247a13afbb3ed3bb632415c763fe846589fbe8
Nick Zhou <zhou.nicholas@outlook.com>
2023-09-29 17:07:31 -0400

    working firebase oauth using local storage to track token

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if(!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+      setToken(token)
+    }
+  })
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Vibology</h1>
+        {!token ? (
+          <button class="LoginButton"
+            onClick={() => {
+              window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
+            }}
+          >
+            Login to Spotify
+          </button>
+        ) : <button onClick={logout}>Logout</button>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
